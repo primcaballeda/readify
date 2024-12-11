790fcb872157dda5de4276d99b2e98220729598f
primcaballeda <p.caballeda.539159@umindanao.edu.ph>
2024-12-11 16:29:41 +0800

    sajdhjash

---
+++ lib/add_review.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class addReview extends StatefulWidget {
+  const addReview({super.key});
+
+  @override
+  State<addReview> createState() => _addReviewState();
+}
+
+class _addReviewState extends State<addReview> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
